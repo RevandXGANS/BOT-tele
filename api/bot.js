d4eebd0b72d87fd384a80c9143abf84b7159dec6
RevandXST <135599316+RevandXGANS@users.noreply.github.com>
2026-04-17 22:53:45 +0700

    Update bot.js

--- api/bot.js
+++ api/bot.js
@@ -29,7 +29,7 @@ app.post('/api/bot', async (req, res) => {
 
 app.get('/set-webhook', async (req, res) => {
   try {
-    const vercelUrl = 'https://script-bot-teleram-vercel.vercel.app';
+    const vercelUrl = 'https://bot-tele-umber.vercel.app/';
     const webhookUrl = `${vercelUrl}/api/bot`;
     
     await bot.deleteWebHook();
